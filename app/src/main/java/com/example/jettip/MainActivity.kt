40a03f38fee18e3832d552ec6a4963ffa1f96665
Nicoka11 <nicoka11@yahoo.fr>
2023-06-30 01:32:54 +0200

    feat: move app to separate composable

--- app/src/main/java/com/example/jettip/MainActivity.kt
+++ app/src/main/java/com/example/jettip/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jettip
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -11,33 +12,38 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.jettip.ui.theme.JetTipTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetTipTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.primaryContainer
-                ) {
-                    Box(modifier = Modifier.padding(all = 16.dp)) {
-                        HeaderCard(title = "Total Per Person :", value = 66.0F)
-                    }
-                }
+            // A surface container using the 'background' color from the theme
+            App {}
+        }
+    }
+}
+
+@Composable
+fun App(content: @Composable () -> Unit) {
+    JetTipTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.primaryContainer
+        ) {
+            Box(modifier = Modifier.padding(all = 16.dp)) {
+                HeaderCard(title = "Total Per Person :", value = 66.0F)
             }
+            content()
         }
     }
 }
@@ -49,7 +55,7 @@ fun HeaderCard(title: String = "Total per person", value: Float = 12.0F) {
         modifier = Modifier
             .fillMaxWidth()
             .height(IntrinsicSize.Min),
-        color = MaterialTheme.colorScheme.secondary,
+        color = MaterialTheme.colorScheme.secondaryContainer,
         shape = MaterialTheme.shapes.large
     ) {
         Column(
@@ -59,14 +65,19 @@ fun HeaderCard(title: String = "Total per person", value: Float = 12.0F) {
         ) {
             Text(
                 text = title,
-                style = TextStyle(color = MaterialTheme.colorScheme.onSecondary),
-                fontSize = 22.sp
+                style = MaterialTheme.typography.titleLarge,
             )
             Text(
                 text = "$$value",
-                style = TextStyle(color = MaterialTheme.colorScheme.onSecondary),
-                fontSize = 32.sp
+                style = MaterialTheme.typography.headlineLarge,
             )
+            Button(
+                onClick = { Log.d("oui", "HeaderCard: this is a click") }
+            ) {
+                Text(
+                    text = "Click on me",
+                )
+            }
         }
     }
 }
