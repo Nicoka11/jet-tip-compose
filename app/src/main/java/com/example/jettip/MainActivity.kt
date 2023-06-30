a4a408ad7389d6eb13c835b0cb64bffe532731b5
Nicoka11 <nicoka11@yahoo.fr>
2023-06-30 22:04:32 +0200

    feat: main container styling

--- app/src/main/java/com/example/jettip/MainActivity.kt
+++ app/src/main/java/com/example/jettip/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.jettip
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -34,28 +32,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun App(content: @Composable () -> Unit) {
+fun App(children: @Composable () -> Unit) {
     JetTipTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.primaryContainer
         ) {
-            Box(modifier = Modifier.padding(all = 16.dp)) {
+            Column(
+                modifier = Modifier.padding(all = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
                 HeaderCard(title = "Total Per Person :", value = 66.0F)
+                MainContainer()
             }
-            content()
+            children()
         }
     }
 }
 
 @Preview
 @Composable
+fun MainContainer() {
+    Surface(
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary),
+        color = MaterialTheme.colorScheme.primaryContainer,
+        shape = MaterialTheme.shapes.large
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(all = 4.dp)
+                .fillMaxWidth()
+                .height(128.dp)
+        ) {
+        }
+    }
+}
+
+@Composable
 fun HeaderCard(title: String = "Total per person", value: Float = 12.0F) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(IntrinsicSize.Min),
-        color = MaterialTheme.colorScheme.secondaryContainer,
+        color = MaterialTheme.colorScheme.primary,
         shape = MaterialTheme.shapes.large
     ) {
         Column(
@@ -71,13 +90,6 @@ fun HeaderCard(title: String = "Total per person", value: Float = 12.0F) {
                 text = "$$value",
                 style = MaterialTheme.typography.headlineLarge,
             )
-            Button(
-                onClick = { Log.d("oui", "HeaderCard: this is a click") }
-            ) {
-                Text(
-                    text = "Click on me",
-                )
-            }
         }
     }
 }
